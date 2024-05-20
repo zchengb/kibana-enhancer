4235a2a8cb01d88a7269cbefcdd51d83b5355036
xiaobin <xiaobin.zheng@thoughtworks.com>
2024-05-20 18:11:33 +0800

    refactor: support to search condition in selector.

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,72 +1,115 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { loadQueryConditions, saveQueryConditions } from '../store';
+import { Cascader } from 'antd';
+import DOMPurify from 'dompurify';
 
-const selectorChangeListener = () => {
-  let selectedValue = getSelectorElement().value;
-
-  const params = new Set();
-  const regex = /{([\w\u4e00-\u9fa5]+)}/g;
-  let match;
-
-  while ((match = regex.exec(selectedValue))?.length > 0) {
-    params.add(match[1]);
-  }
-
-  if (params.size > 0) {
-    const userInputs = promptForInputs(params);
-
-    if (userInputs) {
-      params.forEach((param) => {
-        selectedValue = selectedValue.replaceAll(
-          `{${param}}`,
-          userInputs[param]
-        );
-      });
-    }
-  } else {
-    console.log('No parameters found in the template.');
-  }
-
-  const searchInput = getSearchInputElement();
-  if (searchInput) {
-    searchInput.value = selectedValue;
-    searchInput.textContent = selectedValue;
-
-    const event = new Event('input', { bubbles: true });
-    searchInput.dispatchEvent(event);
-
-    const changeEvent = new Event('change', { bubbles: true });
-    searchInput.dispatchEvent(changeEvent);
-  }
+const injectQuerySelectorOptions = (setOptions) => {
+  loadQueryConditions().then((data) => {
+    const options = data.map((optionItem, index) => ({
+      label: optionItem.label,
+      value: optionItem.value,
+      key: index,
+    }));
+    setOptions(options);
+    console.log('successfully load query conditions.');
+  });
 };
 
-const injectQuerySelectorOptions = (selectorElement) => {
-  queryConditions.forEach((optionItem) => {
-    const option = document.createElement('option');
-    option.textContent = optionItem.label;
-    option.value = optionItem.value;
-    selectorElement.appendChild(option);
+const decodeHTMLEntities = (text) => {
+  const parser = new DOMParser();
+  const dom = parser.parseFromString(text, 'text/html');
+  return dom.body.textContent;
+};
+
+const formatTableContent = () => {
+  const tableCells = document.querySelectorAll('.truncate-by-height:not([data-formatted="true"])');
+  console.log('formatTableContent start:', tableCells.length);
+  tableCells.forEach((cell) => {
+    cell.innerHTML = DOMPurify.sanitize(decodeHTMLEntities(cell.innerHTML));
+    cell.setAttribute('data-formatted', 'true');
   });
+};
+
+const debounce = (func, wait) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, args), wait);
+  };
+};
+
+const debouncedFormatTableContent = debounce(formatTableContent, 2000);
+
+const ConditionSelector = () => {
+  const onChange = (value, selectedOptions) => {
+    const selectedOption = selectedOptions[0];
+    let selectedValue = selectedOption.value;
+
+    const params = new Set();
+    const regex = /{([\w\u4e00-\u9fa5]+)}/g;
+    let match;
+
+    while ((match = regex.exec(selectedValue))?.length > 0) {
+      params.add(match[1]);
+    }
+
+    if (params.size > 0) {
+      const userInputs = promptForInputs(params);
+
+      if (userInputs) {
+        params.forEach((param) => {
+          selectedValue = selectedValue.replaceAll(
+            `{${param}}`,
+            userInputs[param]
+          );
+        });
+      }
+    } else {
+      console.log('No parameters found in the template.');
+    }
+
+    const searchInput = getSearchInputElement();
+    if (searchInput) {
+      searchInput.value = selectedValue;
+      searchInput.textContent = selectedValue;
+
+      const event = new Event('input', { bubbles: true });
+      searchInput.dispatchEvent(event);
+
+      const changeEvent = new Event('change', { bubbles: true });
+      searchInput.dispatchEvent(changeEvent);
+    }
+  };
+
+  const filter = (inputValue, path) =>
+    path.some(
+      (option) =>
+        option.label.toLowerCase().indexOf(inputValue.toLowerCase()) > -1
+    );
+
+  return (
+    <Cascader
+      className={'queryCondition'}
+      options={queryConditions}
+      onChange={onChange}
+      placeholder="Select Query Condition"
+      showSearch={{ filter }}
+    />
+  );
 };
 
 const addConditionSelector = () => {
   const queryContainer = getQueryFilterBarElement();
 
   if (queryContainer) {
-    const selectElement = document.createElement('select');
-    selectElement.id = 'queryConditionSelector';
-    selectElement.classList.add('queryCondition');
+    const selectContainer = document.createElement('div');
+    selectContainer.id = 'queryConditionSelector';
 
-    const placeholderOption = document.createElement('option');
-    placeholderOption.textContent = 'Select Query Condition';
-    placeholderOption.disabled = true;
-    placeholderOption.selected = true;
-    selectElement.appendChild(placeholderOption);
+    queryContainer.insertBefore(selectContainer, queryContainer.lastChild);
 
-    injectQuerySelectorOptions(selectElement);
-
-    queryContainer.insertBefore(selectElement, queryContainer.lastChild);
+    ReactDOM.render(<ConditionSelector />, selectContainer);
     hasInitialized = true;
-    selectElement.addEventListener('change', selectorChangeListener);
     console.log('successfully inject input selector.');
   }
 };
@@ -130,17 +173,12 @@ const addSaveQueryConditionButton = () => {
 };
 
 const refreshSelectorOptions = () => {
-  const queryConditionSelector = getSelectorElement();
-  removeAllOptionsExceptFirst(queryConditionSelector);
-  injectQuerySelectorOptions(queryConditionSelector);
-};
-
-const removeAllOptionsExceptFirst = (selectElement) => {
-  const options = selectElement.querySelectorAll('option');
-
-  for (let i = options.length - 1; i > 0; i--) {
-    selectElement.removeChild(options[i]);
-  }
+  injectQuerySelectorOptions((options) => {
+    const selectContainer = getSelectorElement();
+    if (selectContainer) {
+      ReactDOM.render(<ConditionSelector />, selectContainer);
+    }
+  });
 };
 
 const saveQueryCondition = () => {
@@ -170,21 +208,55 @@ const addEventListenerOnSearchInput = () => {
   const searchInputElement = getSearchInputElement();
   if (searchInputElement) {
     searchInputElement.addEventListener('change', () => {
-      getSelectorElement().options[0].selected = true;
+      const selectContainer = getSelectorElement();
+      if (selectContainer) {
+        ReactDOM.render(<ConditionSelector />, selectContainer);
+      }
     });
     console.log('Successfully add event listener on search input element.');
   }
 };
 
-const observer = new MutationObserver((mutations) => {
+const getTableElement = () => {
+  return document.querySelector('.kbn-table.table');
+};
+
+const rootObserver = new MutationObserver((mutations) => {
   if (isDiscoverPage() && !hasInitialized) {
     addConditionSelector();
     addSaveQueryConditionButton();
     addEventListenerOnSearchInput();
   }
+
+  if (isDiscoverPage() && !addHookOnLogTable) {
+    const tableElement = getTableElement();
+    if (tableElement) {
+      logTableObserver.observe(tableElement, {
+        childList: true,
+        subtree: true,
+        attributes: false,
+        characterData: false,
+      });
+      addHookOnLogTable = true;
+      console.log('Successfully add format hook on log table');
+    }
+  }
 });
 
-const observerOptions = {
+const logTableObserver = new MutationObserver((mutations) => {
+  if (isDiscoverPage()) {
+    mutations.forEach((mutation) => {
+      if (
+        mutation.type === 'childList' &&
+        mutation.target.closest('.kbn-table.table')
+      ) {
+        debouncedFormatTableContent();
+      }
+    });
+  }
+});
+
+const rootObserverOptions = {
   childList: true,
   subtree: true,
   attributes: true,
@@ -192,9 +264,10 @@ const observerOptions = {
 };
 
 let hasInitialized = false;
+let addHookOnLogTable = false;
 let queryConditions = [];
 loadQueryConditions().then((data) => {
   queryConditions = data;
   console.log('successfully load query conditions.');
 });
-observer.observe(document, observerOptions);
+rootObserver.observe(document, rootObserverOptions);
